9021708be9c7d371aa6e40958a2e6a7b5b068c84
DieroKreator <qtechwired@gmail.com>
2024-12-11 16:43:25 -0300

    first commit

---
+++ src/test/java/SelectProductTest.java
@@ -0,0 +1,58 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SelectProductTest {
+
+  private WebDriver driver;
+
+  @BeforeEach
+  public void setUp() {
+    driver = new ChromeDriver();
+    driver.get("https://www.saucedemo.com/");
+    driver.manage().window().maximize();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    driver.quit();
+  }
+
+  @Test
+  public void selecionarProduto() {
+    driver.findElement(By.cssSelector("*[data-test=\"username\"]")).sendKeys("standard_user");
+    driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
+    driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
+    driver.findElement(By.cssSelector("*[data-test=\"inventory-item-name\"]")).click();
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"back-to-products\"]")).getText(), "Back to products");
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"inventory-item-name\"]")).getText(), "Sauce Labs Backpack");
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"inventory-item-price\"]")).getText(), "$29.99");
+    driver.findElement(By.cssSelector("*[data-test=\"add-to-cart\"]")).click();
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-badge\"]")).getText(), "1");
+    driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-link\"]")).click();
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Your Cart");
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"item-quantity\"]")).getText(), "1");
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"inventory-item-name\"]")).getText(), "Sauce Labs Backpack");
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"inventory-item-price\"]")).getText(), "$29.99");
+    driver.findElement(By.cssSelector("*[data-test=\"remove-sauce-labs-backpack\"]")).click();
+    driver.findElement(By.id("react-burger-menu-btn")).click();
+    
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(4000));
+    // driver.findElement(By.cssSelector("*[data-test=\"logout-sidebar-link\"]")).click();
+    WebElement logoutLink = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("*[data-test=\"logout-sidebar-link\"]")));
+    logoutLink.click();
+
+    driver.close();
+  }
+}
---
+++ src/test/resources/features/CompraProdutoBDD.feature
@@ -0,0 +1,19 @@
+# language: pt
+Funcionalidade: Compra Produto
+    Selecionar e comprar um produto
+
+  Cenario: Comprar um produto com sucesso DDT
+    Dado que acesso o site "https://www.saucedemo.com/"
+    E faço login
+    E seleciono o produto <produto> com valor <preço>
+    E adiciono ao carrinho
+    E confirmo o pedido
+    E preencho as informações pessoais
+    E valido as informações do pedido
+    Quando clico no botão de confirmação
+    Entao o pedido é gerado com sucesso
+
+    # Exemplos:
+    #   | produto               | preço    |
+    #   | "Sauce Labs Backpack" | "$29.99" |
+    #   | "Sauce Labs Onesie"   | "$7.99"  |
