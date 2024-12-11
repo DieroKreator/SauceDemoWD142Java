35e771fef166d88b82761b744fb815eede733264
DieroKreator <qtechwired@gmail.com>
2024-12-11 16:45:26 -0300

    Added steps

--- src/test/java/steps/CompraProdutoBDD.java
+++ src/test/java/steps/CompraProdutoBDD.java
@@ -1,5 +1,41 @@
 package steps;
 
 public class CompraProdutoBDD {
-    
+
+    @Dado("que acesso o site {string}")
+    public void que_acesso_o_site(String string) {
+    }
+
+    @Dado("faço login")
+    public void faço_login() {
+    }
+
+    @Dado("seleciono o produto <produto> com valor <preço>")
+    public void seleciono_o_produto_produto_com_valor_preço() {
+    }
+
+    @Dado("adiciono ao carrinho")
+    public void adiciono_ao_carrinho() {
+    }
+
+    @Dado("confirmo o pedido")
+    public void confirmo_o_pedido() {
+    }
+
+    @Dado("preencho as informações pessoais")
+    public void preencho_as_informações_pessoais() {
+    }
+
+    @Dado("valido as informações do pedido")
+    public void valido_as_informações_do_pedido() {
+    }
+
+    @Quando("clico no botão de confirmação")
+    public void clico_no_botão_de_confirmação() {
+    }
+
+    @Entao("o pedido é gerado com sucesso")
+    public void o_pedido_é_gerado_com_sucesso() {
+    }
+
 }
