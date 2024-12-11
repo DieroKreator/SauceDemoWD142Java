40f4aa99a6af3aa2a49295db715191fc37552f98
DieroKreator <qtechwired@gmail.com>
2024-12-11 18:54:54 -0300

    Implemented steps until the inventory item page

--- src/test/java/steps/CompraProdutoBDD.java
+++ src/test/java/steps/CompraProdutoBDD.java
@@ -1,32 +1,70 @@
 package steps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CompraProdutoBDD {
 
+    private WebDriver driver;
+
     @Dado("que acesso o site {string}")
-    public void que_acesso_o_site(String string) {
+    public void que_acesso_o_site(String url) {
+        driver.get(url);
     }
 
-    @Dado("faço login")
+    @E("faço login")
     public void faço_login() {
+        WebElement usernameField = driver.findElement(By.cssSelector("*[data-test=\"username\"]"))
+                .sendKeys("standard_user");
+        WebElement password = driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
+
+        WebElement loginBtn = driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
+
+        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
     }
 
-    @Dado("seleciono o produto <produto> com valor <preço>")
-    public void seleciono_o_produto_produto_com_valor_preço() {
+    @E("seleciono o produto <produto> com valor <preço>")
+    public void seleciono_o_produto_produto_com_valor_preço(String nomeProduto, String preço) {
+        WebElement itemLabel = driver
+                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto + "']"));
+        WebElement itemPrice = driver
+                .findElement(By.xpath("//div[@class='inventory_item_price' and text()='" + preço + "']"));
+        assertEquals(itemLabel.getText(), nomeProduto);
+        assertEquals(itemPrice.getText(), preço);
+
+        itemLabel.click();
+
+        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"back-to-products\"]")).getText(),
+                "Back to products");
+        assertEquals(itemLabel.getText(),
+                nomeProduto);
+        assertEquals(itemPrice.getText(), preço);
     }
 
-    @Dado("adiciono ao carrinho")
+    @E("adiciono ao carrinho")
     public void adiciono_ao_carrinho() {
     }
 
-    @Dado("confirmo o pedido")
+    @E("confirmo o pedido")
     public void confirmo_o_pedido() {
     }
 
-    @Dado("preencho as informações pessoais")
+    @E("preencho as informações pessoais")
     public void preencho_as_informações_pessoais() {
     }
 
-    @Dado("valido as informações do pedido")
+    @E("valido as informações do pedido")
     public void valido_as_informações_do_pedido() {
     }
 
