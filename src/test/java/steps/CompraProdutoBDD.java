90eadede29143b7fcf14aa0520f66872f8307499
DieroKreator <qtechwired@gmail.com>
2024-12-12 14:07:54 -0300

    Undefined error fix on step file

--- src/test/java/steps/CompraProdutoBDD.java
+++ src/test/java/steps/CompraProdutoBDD.java
@@ -1,153 +1,166 @@
 package steps;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.time.Duration;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
 
 public class CompraProdutoBDD {
 
-    private WebDriver driver;
+        private WebDriver driver;
 
-    @Before
-    public void iniciar(){
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
-        driver.manage().window().maximize();
-    }
+        @Before
+        public void iniciar() {
+                driver = new ChromeDriver();
+                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+                driver.manage().window().maximize();
+        }
 
-    @After
-    public void finalizar(){
-        driver.quit();
-    }
+        @After
+        public void finalizar() {
+                driver.quit();
+        }
 
-    @Dado("que acesso o site {string}")
-    public void que_acesso_o_site(String url) {
-        driver.get(url);
-    }
+        @Dado("que acesso o site {string}")
+        public void que_acesso_o_site(String url) {
+                driver.get(url);
+        }
 
-    @E("faço login")
-    public void faço_login() {
-        WebElement usernameField = driver.findElement(By.cssSelector("*[data-test=\"username\"]"))
-                .sendKeys("standard_user");
-        WebElement password = driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
+        @E("faço login")
+        public void faço_login() {
 
-        WebElement loginBtn = driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
+                WebElement usernameField = driver.findElement(By.cssSelector("*[data-test=\"username\"]"));
+                usernameField.sendKeys("standard_user");
 
-        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
-    }
+                WebElement password = driver.findElement(By.cssSelector("*[data-test=\"password\"]"));
+                password.sendKeys("secret_sauce");
 
-    @E("seleciono o produto <produto> com valor <preço>")
-    public void seleciono_o_produto_produto_com_valor_preço(String nomeProduto, String preço) {
-        WebElement itemLabel = driver
-                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto + "']"));
-        WebElement itemPrice = driver
-                .findElement(By.xpath("//div[@class='inventory_item_price' and text()='" + preço + "']"));
-        assertEquals(itemLabel.getText(), nomeProduto);
-        assertEquals(itemPrice.getText(), preço);
+                WebElement loginBtn = driver.findElement(By.cssSelector("*[data-test=\"login-button\"]"));
+                loginBtn.click();
 
-        itemLabel.click();
+                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
+        }
 
-        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"back-to-products\"]")).getText(),
-                "Back to products");
-        assertEquals(itemLabel.getText(),
-                nomeProduto);
-        assertEquals(itemPrice.getText(), preço);
-    }
+        @E("seleciono o produto {string} com valor {string}")
+        public void seleciono_o_produto_com_valor(String nomeProduto, String preço) {
 
-    @E("adiciono ao carrinho")
-    public void adiciono_ao_carrinho() {
+                WebElement itemLabel = driver
+                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto
+                                                + "']"));
+                WebElement itemPrice = driver
+                                .findElement(By.xpath(
+                                                "//div[@class='inventory_item_price' and text()='" + preço + "']"));
+                assertEquals(itemLabel.getText(), nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
 
-        WebElement addBtn = driver.findElement(By.cssSelector("*[data-test=\"add-to-cart\"]"));
-        addcartBtn.click();
+                itemLabel.click();
 
-        WebElement removeBtn = driver.findElement(By.cssSelector("*[data-test=\"remove\"]"));
-        removeBtn.isDisplayed();
+                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"back-to-products\"]")).getText(),
+                                "Back to products");
+                assertEquals(itemLabel.getText(),
+                                nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
+        }
 
-        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-badge\"]")).getText(), "1");
+        @E("adiciono ao carrinho")
+        public void adiciono_ao_carrinho() {
 
-        WebElement cartLink = driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-link\"]"));
-        cartLink.click();
+                WebElement addBtn = driver.findElement(By.cssSelector("*[data-test=\"add-to-cart\"]"));
+                addBtn.click();
 
-        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Your Cart");
-        assertEquals(driver.findElement(By.cssSelector("*[data-test=\"item-quantity\"]")).getText(), "1");
-    }
+                WebElement removeBtn = driver.findElement(By.cssSelector("*[data-test=\"remove\"]"));
+                removeBtn.isDisplayed();
 
-    @E("confirmo o pedido do item <produto> com valor <preço>")
-    public void confirmo_o_pedido_do_item_item_com_valor_preço(String nomeProduto, String preço) {
+                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-badge\"]")).getText(), "1");
 
-        WebElement itemLabel = driver
-                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto + "']"));
-        WebElement itemPrice = driver
-                .findElement(By.xpath("//div[@class='inventory_item_price' and text()='" + preço + "']"));
+                WebElement cartLink = driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-link\"]"));
+                cartLink.click();
 
-        assertEquals(itemLabel.getText(), nomeProduto);
-        assertEquals(itemPrice.getText(), preço);
+                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Your Cart");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"item-quantity\"]")).getText(), "1");
+        }
 
-        WebElement checkoutBtn = driver
-                .findElement(By.css("button[data-test=\"checkout\"]"));
-        checkoutBtn.click();
+        @E("confirmo o pedido do item {string} com valor {string}")
+        public void confirmo_o_pedido_do_item_com_valor(String nomeProduto, String preço) {
 
-        assertEquals(driver.findElement(
-                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Your Information");
-    }
+                WebElement itemLabel = driver
+                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto
+                                                + "']"));
+                WebElement itemPrice = driver
+                                .findElement(By.xpath(
+                                                "//div[@class='inventory_item_price' and text()='" + preço + "']"));
 
-    @E("preencho as informações pessoais")
-    public void preencho_as_informações_pessoais() {
+                assertEquals(itemLabel.getText(), nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
 
-        WebElement firstnameField = driver.findElement(By.id("first-name"));
-        firstnameField.sendKeys("Usuario");
+                WebElement checkoutBtn = driver.findElement(By.cssSelector("button[data-test='checkout']"));
+                checkoutBtn.click();
 
-        WebElement lastnameField = driver.findElement(By.id("last-name"));
-        lastnameField.sendKeys("Test");
+                assertEquals(driver.findElement(
+                                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Your Information");
+        }
 
-        WebElement zipcodeField = driver.findElement(By.id("postal-code"));
-        lastnameField.sendKeys("92158485");
+        @E("preencho as informações pessoais")
+        public void preencho_as_informações_pessoais() {
 
-        WebElement continueBtn = driver
-                .findElement(By.id("continue"));
-        checkoutBtn.click();
+                WebElement firstnameField = driver.findElement(By.id("first-name"));
+                firstnameField.sendKeys("Usuario");
 
-        assertEquals(driver.findElement(
-                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Overview");
-    }
+                WebElement lastnameField = driver.findElement(By.id("last-name"));
+                lastnameField.sendKeys("Test");
 
-    @E("valido as informações do pedido com <produto> e <preço>")
-    public void valido_as_informações_do_pedido_com_produto_e_preço(String nomeProduto, String preço) {
+                WebElement zipcodeField = driver.findElement(By.id("postal-code"));
+                lastnameField.sendKeys("92158485");
 
-        WebElement itemLabel = driver
-                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto + "']"));
-        WebElement itemPrice = driver
-                .findElement(By.xpath("//div[@class='inventory_item_price' and text()='" + preço + "']"));
+                WebElement continueBtn = driver.findElement(By.id("continue"));
+                continueBtn.click();
 
-        assertEquals(itemLabel.getText(), nomeProduto);
-        assertEquals(itemPrice.getText(), preço);
-    }
+                assertEquals(driver.findElement(
+                                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Overview");
+        }
 
-    @Quando("clico no botão de confirmação")
-    public void clico_no_botão_de_confirmação() {
+        @E("valido as informações do pedido com {string} e {string}")
+        public void valido_as_informações_do_pedido_com_e(String string, String string2) {
+                
+                WebElement itemLabel = driver
+                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto
+                                                + "']"));
+                WebElement itemPrice = driver
+                                .findElement(By.xpath(
+                                                "//div[@class='inventory_item_price' and text()='" + preço + "']"));
 
-        WebElement finishBtn = driver
-                .findElement(By.id("finish"));
-        finishBtn.click();
+                assertEquals(itemLabel.getText(), nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
+        }
 
-        assertEquals(driver.findElement(
-                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Complete!");
-    }
+        @Quando("clico no botão de confirmação")
+        public void clico_no_botão_de_confirmação() {
 
-    @Entao("o pedido é gerado com sucesso")
-    public void o_pedido_é_gerado_com_sucesso() {
-        assertEquals(driver.findElement(
-                By.cssSelector("*[data-test=\"complete-header\"]")).getText(), "Thank you for your order!");
-    }
+                WebElement finishBtn = driver
+                                .findElement(By.id("finish"));
+                finishBtn.click();
+
+                assertEquals(driver.findElement(
+                                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Complete!");
+        }
+
+        @Entao("o pedido é gerado com sucesso")
+        public void o_pedido_é_gerado_com_sucesso() {
+                
+                assertEquals(driver.findElement(
+                                By.cssSelector("*[data-test=\"complete-header\"]")).getText(),
+                                "Thank you for your order!");
+        }
 
 }
