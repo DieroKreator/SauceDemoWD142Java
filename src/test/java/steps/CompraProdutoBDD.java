59049873ad13e6c1c4cc53bcc2aa24501887ee0b
DieroKreator <qtechwired@gmail.com>
2024-12-13 15:52:20 -0300

    CompraprodutoBDD test scenario run successfully

--- src/test/java/steps/CompraProdutoBDD.java
+++ src/test/java/steps/CompraProdutoBDD.java
@@ -2,7 +2,10 @@ package steps;
 
 import static org.junit.Assert.assertEquals;
 
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -18,7 +21,9 @@ import io.cucumber.java.pt.Quando;
 
 public class CompraProdutoBDD {
 
-        private WebDriver driver;
+        WebDriver driver;
+        String nomeProduto;
+        String preço;
 
         @Before
         public void iniciar() {
@@ -52,54 +57,159 @@ public class CompraProdutoBDD {
                 assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
         }
 
+        // @E("seleciono o produto {string} com valor {string}")
+        // public void seleciono_o_produto_com_valor(String nomeProduto, String preço) {
+
+        // WebElement itemLabel;
+        // WebElement itemPrice;
+        // {
+        // this.nomeProduto = nomeProduto;
+        // itemLabel = driver
+        // .findElement(By.xpath("//div[@data-test='inventory-item-name' and text()='"
+        // + nomeProduto + "']"));
+        // }
+
+        // {
+        // this.preço = preço;
+        // // itemPrice = driver
+        // // .findElement(By.xpath("//div[@class='inventory_item_price' and
+        // contains(text(),'" + preço + "')]"));
+
+        // List<WebElement> items =
+        // driver.findElements(By.xpath("//div[@data-test='inventory-item-price']"));
+
+        // for (WebElement item : items) {
+        // if (item.getText() == preço)
+        // break;
+        // }
+
+        // }
+
+        // // .findElement(By.xpath("//div[text()='" + nomeProduto
+        // // +
+        // //
+        // "']/ancestor::div[@class='inventory_item']//div[@class='inventory_item_price']"));
+        // // .findElement(By.cssSelector("*[data-test=\"inventory-item-price\"]"));
+        // // .findElement(By.xpath("(//div[@class='inventory_item_price'])[1]"));
+
+        // assertEquals(itemLabel.getText(), nomeProduto);
+        // assertEquals(itemPrice.getText(), preço);
+
+        // itemLabel.click();
+
+        // assertEquals(driver.findElement(By.cssSelector("*[data-test='back-to-products']")).getText(),
+        // "Back to products");
+        // assertEquals(itemLabel.getText(), nomeProduto);
+        // assertEquals(itemPrice.getText(), preço);
+        // }
         @E("seleciono o produto {string} com valor {string}")
         public void seleciono_o_produto_com_valor(String nomeProduto, String preço) {
 
-                WebElement itemLabel = driver
-                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto
-                                                + "']"));
-                WebElement itemPrice = driver
-                                .findElement(By.xpath(
-                                                "//div[@class='inventory_item_price' and text()='" + preço + "']"));
+                WebElement itemLabel;
+                WebElement itemPrice;
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+                {
+                        this.nomeProduto = nomeProduto;
+                        itemLabel = driver
+                                        .findElement(By.xpath("//div[@data-test='inventory-item-name' and text()='"
+                                                        + nomeProduto + "']"));
+                }
+
+                {
+                        this.preço = preço;
+
+                        itemPrice = itemLabel.findElement(By.xpath(
+                                        "./ancestor::div[@class='inventory_item']//div[@class='inventory_item_price']"));
+
+                        // Retrieve and combine the price parts dynamically
+                        String[] priceParts = itemPrice.getText().split("\n");
+                        String fullPrice = String.join("", priceParts); // Combines all parts of the price
+
+                        // Validate the full price
+                        System.out.println("Price for " + nomeProduto + ": " + fullPrice);
+                        if (fullPrice.equals(preço)) {
+                                System.out.println("Price is correct.");
+                        } else {
+                                System.out.println("Price is incorrect. Found: " + fullPrice);
+                        }
+
+                }
+
                 assertEquals(itemLabel.getText(), nomeProduto);
                 assertEquals(itemPrice.getText(), preço);
 
                 itemLabel.click();
 
-                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"back-to-products\"]")).getText(),
+                // Wait for the element to be visible and fetch its text
+                WebElement backToProductsButton = wait.until(
+                                ExpectedConditions.visibilityOfElementLocated(
+                                                By.cssSelector("*[data-test='back-to-products']")));
+
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='back-to-products']")).getText(),
                                 "Back to products");
-                assertEquals(itemLabel.getText(),
-                                nomeProduto);
-                assertEquals(itemPrice.getText(), preço);
+                // assertEquals(itemLabel.getText(), nomeProduto);
+                // assertEquals(itemPrice.getText(), preço);
         }
 
         @E("adiciono ao carrinho")
         public void adiciono_ao_carrinho() {
 
-                WebElement addBtn = driver.findElement(By.cssSelector("*[data-test=\"add-to-cart\"]"));
+                WebElement addBtn = driver.findElement(By.id("add-to-cart"));
                 addBtn.click();
 
-                WebElement removeBtn = driver.findElement(By.cssSelector("*[data-test=\"remove\"]"));
+                WebElement removeBtn = driver.findElement(By.cssSelector("*[data-test='remove']"));
                 removeBtn.isDisplayed();
 
-                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-badge\"]")).getText(), "1");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='shopping-cart-badge']")).getText(), "1");
 
-                WebElement cartLink = driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-link\"]"));
+                WebElement cartLink = driver.findElement(By.cssSelector("*[data-test='shopping-cart-link']"));
                 cartLink.click();
 
-                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Your Cart");
-                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"item-quantity\"]")).getText(), "1");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='title']")).getText(), "Your Cart");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='item-quantity']")).getText(), "1");
+        }
+
+        public void delay(int milliseconds) {
+                try {
+                        Thread.sleep(milliseconds);
+                } catch (InterruptedException e) {
+                        e.printStackTrace();
+                }
         }
 
         @E("confirmo o pedido do item {string} com valor {string}")
         public void confirmo_o_pedido_do_item_com_valor(String nomeProduto, String preço) {
 
-                WebElement itemLabel = driver
-                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto
-                                                + "']"));
-                WebElement itemPrice = driver
-                                .findElement(By.xpath(
-                                                "//div[@class='inventory_item_price' and text()='" + preço + "']"));
+                WebElement itemLabel;
+                WebElement itemPrice;
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+                {
+                        this.nomeProduto = nomeProduto;
+                        itemLabel = driver
+                                        .findElement(By.xpath("//div[@data-test='inventory-item-name' and text()='"
+                                                        + nomeProduto + "']"));
+                }
+
+                {
+                        this.preço = preço;
+
+                        itemPrice = driver.findElement(By.cssSelector("div[data-test='inventory-item-price']"));
+
+                        // Retrieve and combine the price parts dynamically
+                        String[] priceParts = itemPrice.getText().split("\n");
+                        String fullPrice = String.join("", priceParts); // Combines all parts of the price
+
+                        // Validate the full price
+                        System.out.println("Price for " + nomeProduto + ": " + fullPrice);
+                        if (fullPrice.equals(preço)) {
+                                System.out.println("Price is correct.");
+                        } else {
+                                System.out.println("Price is incorrect. Found: " + fullPrice);
+                        }
+
+                }
 
                 assertEquals(itemLabel.getText(), nomeProduto);
                 assertEquals(itemPrice.getText(), preço);
@@ -108,7 +218,7 @@ public class CompraProdutoBDD {
                 checkoutBtn.click();
 
                 assertEquals(driver.findElement(
-                                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Your Information");
+                                By.cssSelector("*[data-test='title']")).getText(), "Checkout: Your Information");
         }
 
         @E("preencho as informações pessoais")
@@ -121,22 +231,41 @@ public class CompraProdutoBDD {
                 lastnameField.sendKeys("Test");
 
                 WebElement zipcodeField = driver.findElement(By.id("postal-code"));
-                lastnameField.sendKeys("92158485");
+                zipcodeField.sendKeys("92158485");
 
                 WebElement continueBtn = driver.findElement(By.id("continue"));
                 continueBtn.click();
 
                 assertEquals(driver.findElement(
-                                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Overview");
+                                By.cssSelector("*[data-test='title']")).getText(), "Checkout: Overview");
         }
 
         @E("valido as informações do pedido com {string} e {string}")
         public void valido_as_informações_do_pedido_com_e(String nomeProduto, String preço) {
-                
+
                 WebElement itemLabel = driver
-                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto + "']"));
-                WebElement itemPrice = driver
-                                .findElement(By.xpath("//div[@class='inventory_item_price' and text()='" + preço + "']"));
+                                .findElement(By.xpath("//div[@class='inventory_item_name' and text()='" + nomeProduto
+                                                + "']"));
+                WebElement itemPrice;
+
+                {
+                        this.preço = preço;
+
+                        itemPrice = driver.findElement(By.cssSelector("div[data-test='inventory-item-price']"));
+
+                        // Retrieve and combine the price parts dynamically
+                        String[] priceParts = itemPrice.getText().split("\n");
+                        String fullPrice = String.join("", priceParts); // Combines all parts of the price
+
+                        // Validate the full price
+                        System.out.println("Price for " + nomeProduto + ": " + fullPrice);
+                        if (fullPrice.equals(preço)) {
+                                System.out.println("Price is correct.");
+                        } else {
+                                System.out.println("Price is incorrect. Found: " + fullPrice);
+                        }
+
+                }
 
                 assertEquals(itemLabel.getText(), nomeProduto);
                 assertEquals(itemPrice.getText(), preço);
@@ -150,14 +279,14 @@ public class CompraProdutoBDD {
                 finishBtn.click();
 
                 assertEquals(driver.findElement(
-                                By.cssSelector("*[data-test=\"title\"]")).getText(), "Checkout: Complete!");
+                                By.cssSelector("*[data-test='title']")).getText(), "Checkout: Complete!");
         }
 
         @Entao("o pedido é gerado com sucesso")
         public void o_pedido_é_gerado_com_sucesso() {
 
                 assertEquals(driver.findElement(
-                                By.cssSelector("*[data-test=\"complete-header\"]")).getText(),
+                                By.cssSelector("*[data-test='complete-header']")).getText(),
                                 "Thank you for your order!");
         }
 
