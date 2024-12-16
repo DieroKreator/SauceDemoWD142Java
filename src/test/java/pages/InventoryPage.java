f7ea92c86e946c49ef57d40ef430ee27d7590be5
DieroKreator <qtechwired@gmail.com>
2024-12-15 22:37:52 -0300

    Added page folder and InventoryPage

---
+++ src/test/java/pages/InventoryPage.java
@@ -0,0 +1,18 @@
+package test.java.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class InventoryPage {
+    
+    public InventoryPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public By byLocal(String local){
+
+        return By.cssSelector("option[value=\"" + local + "\"]");
+    }
+}
