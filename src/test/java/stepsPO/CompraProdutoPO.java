f68c16df39a93132186037f559afe924d400143e
DieroKreator <qtechwired@gmail.com>
2024-12-16 18:05:00 -0300

    Correção na descricao dos steps

--- src/test/java/stepsPO/CompraProdutoPO.java
+++ src/test/java/stepsPO/CompraProdutoPO.java
@@ -1,6 +1,6 @@
 package stepsPO;
 
-import static org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -45,10 +45,10 @@ public class CompraProdutoPO {
                 loginPage = new LoginPage(driver);
                 loginPage.acessarLoginPage(url);
 
-                Assert.assertEquals("Swag Labs", loginPage.lerNomeDaGuia());
+                assertEquals("Swag Labs", loginPage.lerNomeDaGuia());
         }
 
-        @E("faço login_po")
+        @E("faço login PO")
         public void faço_login_po() {
 
                 WebElement usernameField = driver.findElement(By.cssSelector("*[data-test=\"username\"]"));
@@ -60,11 +60,11 @@ public class CompraProdutoPO {
                 WebElement loginBtn = driver.findElement(By.cssSelector("*[data-test=\"login-button\"]"));
                 loginBtn.click();
 
-                Assert.assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test=\"title\"]")).getText(), "Products");
         }
 
         /* TO DO: Implement code for 2 extra products */
-        @E("seleciono o produto {string} com valor {string} po")
+        @E("seleciono o produto {string} com valor {string} PO")
         public void seleciono_o_produto_com_valor_po(String nomeProduto, String preço) {
 
                 WebElement itemLabel;
@@ -98,8 +98,8 @@ public class CompraProdutoPO {
 
                 }
 
-                Assert.assertEquals(itemLabel.getText(), nomeProduto);
-                Assert.assertEquals(itemPrice.getText(), preço);
+                assertEquals(itemLabel.getText(), nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
 
                 itemLabel.click();
 
@@ -108,13 +108,13 @@ public class CompraProdutoPO {
                                 ExpectedConditions.visibilityOfElementLocated(
                                                 By.cssSelector("*[data-test='back-to-products']")));
 
-                Assert.assertEquals(driver.findElement(By.cssSelector("*[data-test='back-to-products']")).getText(),
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='back-to-products']")).getText(),
                                 "Back to products");
                 // assertEquals(itemLabel.getText(), nomeProduto);
                 // assertEquals(itemPrice.getText(), preço);
         }
 
-        @E("adiciono ao carrinho po")
+        @E("adiciono ao carrinho PO")
         public void adiciono_ao_carrinho_po() {
 
                 WebElement addBtn = driver.findElement(By.id("add-to-cart"));
@@ -123,13 +123,13 @@ public class CompraProdutoPO {
                 WebElement removeBtn = driver.findElement(By.cssSelector("*[data-test='remove']"));
                 removeBtn.isDisplayed();
 
-                Assert.assertEquals(driver.findElement(By.cssSelector("*[data-test='shopping-cart-badge']")).getText(), "1");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='shopping-cart-badge']")).getText(), "1");
 
                 WebElement cartLink = driver.findElement(By.cssSelector("*[data-test='shopping-cart-link']"));
                 cartLink.click();
 
-                Assert.assertEquals(driver.findElement(By.cssSelector("*[data-test='title']")).getText(), "Your Cart");
-                Assert.assertEquals(driver.findElement(By.cssSelector("*[data-test='item-quantity']")).getText(), "1");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='title']")).getText(), "Your Cart");
+                assertEquals(driver.findElement(By.cssSelector("*[data-test='item-quantity']")).getText(), "1");
         }
 
         public void delay(int milliseconds) {
@@ -140,7 +140,7 @@ public class CompraProdutoPO {
                 }
         }
 
-        @E("confirmo o pedido do item {string} com valor {string} po")
+        @E("confirmo o pedido do item {string} com valor {string} PO")
         public void confirmo_o_pedido_do_item_com_valor_po(String nomeProduto, String preço) {
 
                 WebElement itemLabel;
@@ -173,8 +173,8 @@ public class CompraProdutoPO {
 
                 }
 
-                Assert.assertEquals(itemLabel.getText(), nomeProduto);
-                Assert.assertEquals(itemPrice.getText(), preço);
+                assertEquals(itemLabel.getText(), nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
 
                 WebElement checkoutBtn = driver.findElement(By.cssSelector("button[data-test='checkout']"));
                 checkoutBtn.click();
@@ -183,7 +183,7 @@ public class CompraProdutoPO {
                                 By.cssSelector("*[data-test='title']")).getText(), "Checkout: Your Information");
         }
 
-        @E("preencho as informações pessoais po")
+        @E("preencho as informações pessoais PO")
         public void preencho_as_informações_pessoais_po() {
 
                 WebElement firstnameField = driver.findElement(By.id("first-name"));
@@ -198,11 +198,11 @@ public class CompraProdutoPO {
                 WebElement continueBtn = driver.findElement(By.id("continue"));
                 continueBtn.click();
 
-                Assert.assertEquals(driver.findElement(
+                assertEquals(driver.findElement(
                                 By.cssSelector("*[data-test='title']")).getText(), "Checkout: Overview");
         }
 
-        @E("valido as informações do pedido com {string} e {string} po")
+        @E("valido as informações do pedido com {string} e {string} PO")
         public void valido_as_informações_do_pedido_com_e_po(String nomeProduto, String preço) {
 
                 WebElement itemLabel = driver
@@ -229,25 +229,25 @@ public class CompraProdutoPO {
 
                 }
 
-                Assert.assertEquals(itemLabel.getText(), nomeProduto);
-                Assert.assertEquals(itemPrice.getText(), preço);
+                assertEquals(itemLabel.getText(), nomeProduto);
+                assertEquals(itemPrice.getText(), preço);
         }
 
-        @Quando("clico no botão de confirmação po")
+        @Quando("clico no botão de confirmação PO")
         public void clico_no_botão_de_confirmação_po() {
 
                 WebElement finishBtn = driver
                                 .findElement(By.id("finish"));
                 finishBtn.click();
 
-                Assert.assertEquals(driver.findElement(
+                assertEquals(driver.findElement(
                                 By.cssSelector("*[data-test='title']")).getText(), "Checkout: Complete!");
         }
 
-        @Entao("o pedido é gerado com sucesso po")
+        @Entao("o pedido é gerado com sucesso PO")
         public void o_pedido_é_gerado_com_sucesso_po() {
 
-                Assert.assertEquals(driver.findElement(
+                assertEquals(driver.findElement(
                                 By.cssSelector("*[data-test='complete-header']")).getText(),
                                 "Thank you for your order!");
         }
