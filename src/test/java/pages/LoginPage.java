d1a6082ce0f80be2009ccff1ccba77c0e376fbda
DieroKreator <qtechwired@gmail.com>
2024-12-16 14:52:24 -0300

    Added LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,3 +1,45 @@
-public class LoginPage {
+package test.java.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends CommonPage{
     
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+    
+    public By byLocal(String local){
+
+        return By.cssSelector("option[value=\"" + local + "\"]");
+    }
+
+    @FindBy(id = "user-name")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    WebElement loginBtn;
+
+    public void acessarLoginPage(String url){
+        driver.get(url);
+    }
+
+    public void preencherUsername(){
+        usernameField.sendKeys("standard_user");
+    }
+
+    public void preencherPassword(){
+        usernameField.sendKeys("secret_sauce");
+    }
+
+    public void clicarBotãoLogin(){
+        loginBtn.click();
+    }
 }
