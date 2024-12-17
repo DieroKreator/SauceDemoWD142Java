54cacd032e8bb1643777b71e83f34daaac9ffc85
DieroKreator <qtechwired@gmail.com>
2024-12-16 21:09:10 -0300

    Mapped Inventory page

--- src/test/java/pages/InventoryPage.java
+++ src/test/java/pages/InventoryPage.java
@@ -1,18 +1,27 @@
 package test.java.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class InventoryPage {
-    
-    // public InventoryPage(WebDriver driver) {
-    //     super(driver);
-    //     PageFactory.initElements(driver, this);
-    // }
 
-    // public By byLocal(String local){
+    public InventoryPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
 
-    //     return By.cssSelector("option[value=\"" + local + "\"]");
-    // }
+    public By byLocal(String local) {
+
+        return By.cssSelector("option[value=\"" + local + "\"]");
+    }
+
+    @FindBy(xpath = "//div[@data-test='inventory-item-name' and text()='" + nomeProduto + "']")
+    WebElement itemLabel;
+
+    public void selecionarProduto(String nomeProduto) {
+        itemLabel.click();
+    }
 }
